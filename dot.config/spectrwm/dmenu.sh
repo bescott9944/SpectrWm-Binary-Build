cd8a3485a014fc14c8ff1e0ffd9b4179f52dd48e
Bruce E. Scott <51040721+bescott9944@users.noreply.github.com>
2020-07-09 20:56:49 -0400

    Add files via upload

---
+++ dot.config/spectrwm/dmenu.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+#
+# Bash file to start Suckless Dmenu
+ /usr/bin/dmenu_run -b
+# -b -fn $bar_font -nb $bar_color -nf $bar_font_color -sb $bar_color_selected -sf $bar_font_color_selected
